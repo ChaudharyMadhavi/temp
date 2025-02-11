3e65457d91220f4519e57b3a457c3550ccc2ed6d
Madhavi chaudhary <132742045+ChaudharyMadhavi@users.noreply.github.com>
2025-02-11 17:41:01 +0530

    Add files via upload

---
+++ ecom-project/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO product (name, description, brand, price, category, releasedate, available, quantity)
+VALUES
+('Smartphone', 'A high-quality smartphone', 'BrandA', 699.99, 'Electronics', '2023-12-01', TRUE, 100),
+('Laptop', 'Lightweight and powerful laptop', 'BrandB', 1299.99, 'Electronics', '2023-11-15', TRUE, 50),
+('Headphones', 'Noise-cancelling headphones', 'BrandC', 199.99, 'Accessories', '2023-10-20', TRUE, 150);
+
+
